4a40ce09bdab641787f5ddf983bbfc702448a6c9
Diya <bishtdiya123@gmail.com>
2024-11-10 23:15:41 +0530

    edit

--- home/views.py
+++ home/views.py
@@ -3,19 +3,19 @@ from .models import Contact  # Import the Contact model
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    return render(request, '../index.html')
 
 def about(request):
-    return render(request, 'about.html')
+    return render(request, '../about.html')
 
 def contact(request):
-   return render(request, 'contact.html')
+   return render(request, '../contact.html')
 
 def practice(request):
     return HttpResponse("This is a practice page")
 
 def course_outline(request):
-    return render(request, 'course_outline.html')
+    return render(request, '../course_outline.html')
 
 def faculty(request):
-    return render(request, 'faculty.html')
+    return render(request, '../faculty.html')
