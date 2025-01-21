bb640ffce4d3c30b9ab4293534047a29ce187af5
Ajayrawat17 <ajayrawat11146@gmail.com>
2025-01-21 22:31:47 +0530

    changes

--- home/views.py
+++ home/views.py
@@ -93,19 +93,19 @@ def login_view(request):
 def btech(request):
     return render(request, '../templates/pyqs/btech pyqs/btech.html')
 def bba(request):
-    return render(request, '../templates/pyqs/mba pyqs/mba.html')
+    return render(request, '../templates/pyqs/bba pyqs/bba.html')
 def mba(request):
-    return render(request, '../templates/mba.html')
+    return render(request, '../templates/pyqs/mba pyqs/mba.html')
 def bsc(request):
     return render(request, '../templates/bsc.html')
 def mca(request):
-    return render(request, '../templates/mca.html')
+    return render(request, '../templates/pyqs/mca pyqs/mca.html')
 def mcom(request):
     return render(request, '../templates/mcom.html')
 def bcom(request):
     return render(request, '../templates/bcom.html')
 def bca(request):
-    return render(request, '../templates/bca.html')
+    return render(request, '../templates/pyqs/bca pyqs/bca.html')
 def btech_assignment(request):
     return render(request, '../templates/btech_assignment.html')    
 
@@ -132,79 +132,79 @@ def sem1physicspyqs(request):
     return render(request, '../templates/pyqs/btech pyqs/sem1-physics-pyq.html')
 
 def sem1ecepyqs(request):
-    return render(request, 'home/templates/pyqs/btech pyqs/sem1-ece-pyq.html')
+    return render(request, '../templates/pyqs/btech pyqs/sem1-ece-pyq.html')
 
 def sem2mathpyqs(request):
-    return render(request, 'home/templates/pyqs/btech pyqs/sem2-math-pyq.html')  
+    return render(request, '../templates/pyqs/btech pyqs/sem2-math-pyq.html')  
 
 def sem2chemistrypyqs(request):
-    return render(request, '../templates/sem2-chemistry-pyq.html') 
+    return render(request, '../templates/pyqs/btech pyqs/sem2-chemistry-pyq.html') 
 
 def sem2evspyqs(request):
-    return render(request, '../templates/sem2-evs-pyq.html') 
+    return render(request, '../templates/pyqs/btech pyqs/sem2-evs-pyq.html') 
 
 def sem3OOPSpyqs(request):
-    return render(request, '../templates/sem3-OOPS-pyq.html') 
+    return render(request, '../templates/pyqs/btech pyqs/sem3-OOPS-pyq.html') 
 
 def sem3dspyqs(request):
-    return render(request, '../templates/sem3-ds-pyq.html') 
+    return render(request, '../templates/pyqs/btech pyqs/sem3-ds-pyq.html') 
 
 def sem3digitallogicpyqs(request):
-    return render(request, '../templates/sem3-digital-logic-pyq.html')
+    return render(request, '../templates/pyqs/btech pyqs/sem3-digital-logic-pyq.html')
 
 def sem4javapyqs(request):
-    return render(request, '../templates/sem4-java-pyq.html')  
+    return render(request, '../templates/pyqs/btech pyqs/sem4-java-pyq.html')  
 
 def sem4micropyqs(request):
-    return render(request, '../templates/sem4-micro-pyq.html')
+    return render(request, '../templates/pyqs/btech pyqs/sem4-micro-pyq.html')
 
 def sem4Automatapyqs(request):
-    return render(request, '../templates/sem4-Automata-pyq.html') 
+    return render(request, '../templates/pyqs/btech pyqs/sem4-Automata-pyq.html') 
 
 def sem5mlpyqs(request):
-    return render(request, '../templates/sem5-ml-pyq.html')  
+    return render(request, '../templates/pyqs/btech pyqs/sem5-ml-pyq.html')  
 
 def sem5databasepyqs(request):
-    return render(request, '../templates/sem5-database-pyq.html') 
+    return render(request, '../templates/pyqs/btech pyqs/sem5-database-pyq.html') 
 
 def sem5ospyqs(request):
-    return render(request, '../templates/sem5-os-pyq.html')  
+    return render(request, '../templates/pyqs/btech pyqs/sem5-os-pyq.html')  
 
 def sem6compilerpyqs(request):
-    return render(request, '../templates/sem6-compiler-pyq.html') 
+    return render(request, '../templates/pyqs/btech pyqs/sem6-compiler-pyq.html') 
 
 def sem6sepyqs(request):
-    return render(request, '../templates/sem6-se-pyq.html')  
+    return render(request, '../templates/pyqs/btech pyqs/sem6-se-pyq.html')  
 
 def sem6networksecuritypyqs(request):
-    return render(request, '../templates/sem6-network-security-pyq.html') 
+    return render(request, '../templates/pyqs/btech pyqs/sem6-network-security-pyq.html') 
 
 def sem7Softpyqs(request):
-    return render(request, '../templates/sem7-s oft-pyq.html')   
+    return render(request, '../templates/pyqs/btech pyqs/sem7-soft-pyq.html')   
 
 def sem7bipyqs(request):
-    return render(request, '../templates/sem7-bi-pyq.html') 
+    return render(request, '../templates/pyqs/btech pyqs/sem7-bi-pyq.html') 
 
 def sem7cgpyqs(request):
-    return render(request, '../templates/sem7-cg-pyq.html') 
+    return render(request, '../templates/pyqs/btech pyqs/sem7-cg-pyq.html') 
 
 def sem8ccpyqs(request):
-    return render(request, '../templates/sem8-cc-pyq.html') 
+    return render(request, '../templates/pyqs/btech pyqs/sem8-cc-pyq.html') 
 
 def sem8mbpyqs(request):
-    return render(request, '../templates/sem8-mb-pyq.html') 
+    return render(request, '../templates/pyqs/btech pyqs/sem8-mb-pyq.html') 
 
 def sem8dmpyqs(request):
-    return render(request, '../templates/sem8-dm-pyq.html') 
+    return render(request, '../templates/pyqs/btech pyqs/sem8-dm-pyq.html') 
 
 def sem1physicspyqs(request):
-    return render(request, '../templates/sem1-physics-pyq.html') 
+    return render(request, '../templates/pyqs/btech pyqs/sem1-physics-pyq.html') 
 
 def sem1chemistrypyqs(request):
     return render(request, '../templates/sem1-chemistry-pyq.html')
 
 def sem1mathpyqs(request):
-    return render(request, '../templates/sem1-math-pyq.html')  
+    return render(request, '../templates/pyqs/btech pyqs/sem1-math-pyq.html')  
 
 def sem1englishpyqs(request):
     return render(request, '../templates/sem1-english-pyq.html') 
@@ -213,10 +213,10 @@ def sem2physicspyqs(request):
     return render(request, '../templates/sem2-physics-pyq.html') 
 
 def sem2chemistrypyqs(request):
-    return render(request, '../templates/sem2-chemistry-pyq.html') 
+    return render(request, '../templates/pyqs/btech pyqs/sem2-chemistry-pyq.html') 
 
 def sem2mathpyqs(request):
-    return render(request, '../templates/sem2-math-pyq.html')  
+    return render(request, '../templates/pyqs/btech pyqs/sem2-math-pyq.html')  
 
 def sem2csbasicspyqs(request):
     return render(request, '../templates/sem2-cs-basics-pyq.html')
@@ -252,10 +252,10 @@ def sem5inorganicchempyqs(request):
     return render(request, '../templates/sem5-inorganic-chem-pyq.html') 
 
 def sem5dbmspyqs(request):
-    return render(request, '../templates/sem5-dbms-pyq.html') 
+    return render(request, '../templates/pyqs/btech pyqs/sem5-database-pyq.html') 
 
 def sem5ospyqs(request):
-    return render(request, '../templates/sem5-os-pyq.html') 
+    return render(request, '../templates/pyqs/btech pyqs/sem5-os-pyq.html') 
 
 def sem6relativitypyqs(request):
     return render(request, '../templates/sem6-relativity-pyq.html') 
@@ -292,96 +292,98 @@ def sem8mlpyqs(request):
 
 def sem8cloudcomputingpyqs(request):
     return render(request, '../templates/sem8-cloud-computing-pyq.html')
+
 def sem1computerfundamental(request):
-    return render(request, '../templates/bca-sem1-computerfundamental-pyq.html') 
+    return render(request, '../templates/pyqs/bca pyqs/bca-sem1-computerfundamental-pyq.html') 
 def sem1maths(request):
-    return render(request, '../templates/bca-sem1-maths-pyq.html') 
+    return render(request, '../templates/pyqs/bca pyqs/bca-sem1-maths-pyq.html') 
 def sem1programminginc(request):
-    return render(request, '../templates/bca-sem1-programming.pyq.html') 
+    return render(request, '../templates/pyqs/bca pyqs/bca-sem1-programming.pyq.html') 
 def sem2maths(request):
-    return render(request, '../templates/bca-sem2-math-pyq.html') 
+    return render(request, '../templates/pyqs/bca pyqs/bca-sem2-math-pyq.html') 
 def sem2datastructure(request):
-    return render(request, '../templates/bca-sem2-datastructure-pyq.html') 
+    return render(request, '../templates/pyqs/bca pyqs/bca-sem2-datastructure-pyq.html') 
 def sem2dbmspyqs(request):
-    return render(request, '../templates/bca-sem2-dbms-pyq.html') 
+    return render(request, '../templates/pyqs/bca pyqs/bca-sem2-dbms-pyq.html') 
 def sem3os(request):
-    return render(request, '../templates/bca-sem3-os-pyq.html') 
+    return render(request, '../templates/pyqs/bca pyqs/bca-sem3-os-pyq.html') 
 def sem3oop(request):
-    return render(request, '../templates/bca-sem3-oop-pyq.html') 
+    return render(request, '../templates/pyqs/bca pyqs/bca-sem3-oop-pyq.html') 
 def sem3computernetwork(request):
-    return render(request, '../templates/bca-sem3-computernetwork-pyq.html') 
+    return render(request, '../templates/pyqs/bca pyqs/bca-sem3-computernetwork-pyq.html') 
 def sem3softwareengineering(request):
-    return render(request, '../templates/bca-sem3-softwareengineering-pyq.html') 
+    return render(request, '../templates/pyqs/bca pyqs/bca-sem3-softwareengineering-pyq.html') 
 def sem4dda(request):
-    return render(request, '../templates/bca-sem4-dda-pyq.html') 
+    return render(request, '../templates/pyqs/bca pyqs/bca-sem4-dda-pyq.html') 
 def sem4softwaredevelopment(request):
-    return render(request, '../templates/bca-sem4-softwaredevelopment-pyq.html') 
+    return render(request, '../templates/pyqs/bca pyqs/bca-sem4-softwaredevelopment-pyq.html') 
 def sem4computerarchietecture(request):
-    return render(request, '../templates/bca-sem4-computerarchietecture-pyq.html') 
+    return render(request, '../templates/pyqs/bca pyqs/bca-sem4-computerarchietecture-pyq.html') 
 def sem4evs(request):
-    return render(request, '../templates/bca-sem4-evs-pyq.html') 
+    return render(request, '../templates/pyqs/bca pyqs/bca-sem4-evs-pyq.html') 
 def sem5webtech(request):
-    return render(request, '../templates/bca-sem5-webtechnologies-pyq.html') 
+    return render(request, '../templates/pyqs/bca pyqs/bca-sem5-webtechnologies-pyq.html') 
 def sem5mobilecomputing(request):
-    return render(request, '../templates/bca-sem5-mobiletech-pyq.html') 
+    return render(request, '../templates/pyqs/bca pyqs/bca-sem5-mobiletech-pyq.html') 
 def sem5networksecurity(request):
-    return render(request, '../templates/bca-sem5-networksequrity-pyq.html') 
+    return render(request, '../templates/pyqs/bca pyqs/bca-sem5-networksequrity-pyq.html') 
 def sem5ai(request):
-    return render(request, '../templates/bca-sem5-AI-pyq.html') 
+    return render(request, '../templates/pyqs/bca pyqs/bca-sem5-AI-pyq.html') 
 def sem6bigdataanalysis(request):
-    return render(request, '../templates/bca-sem6-bigdata-pyq.html') 
+    return render(request, '../templates/pyqs/bca pyqs/bca-sem6-bigdata-pyq.html') 
 def sem6cloudcomputing(request):
-    return render(request, '../templates/bca-sem6-cloudcomputing-pyq.html') 
+    return render(request, '../templates/pyqs/bca pyqs/bca-sem6-cloudcomputing-pyq.html') 
 def sem6projectmanagement(request):
-    return render(request, '../templates/bca-sem6-projectmanagement-pyq.html') 
+    return render(request, '../templates/pyqs/bca pyqs/bca-sem6-projectmanagement-pyq.html') 
 def sem6datascience(request):
-    return render(request, '../templates/bca-sem6-datascience-pyq.html') 
+    return render(request, '../templates/pyqs/bca pyqs/bca-sem6-datascience-pyq.html') 
+
 def bbasem1buisnessmaths(request):
-    return render(request, '../templates/bba-sem1-buisnessmathe-pyq.html') 
+    return render(request, '../templates/pyqs/bba pyqs/bba-sem1-buisnessmathe-pyq.html') 
 def bbasem1finalcialaccounting(request):
-    return render(request, '../templates/bba-sem1-finacialacco-pyq.html') 
+    return render(request, '../templates/pyqs/bba pyqs/bba-sem1-finacialacco-pyq.html') 
 def bbasem1microeco(request):
-    return render(request, '../templates/bba-sem1-microeconomics-pyq.html') 
+    return render(request, '../templates/pyqs/bba pyqs/bba-sem1-microeconomics-pyq.html') 
 def bbasem1principlemanagement(request):
-    return render(request, '../templates/bba-sem1-priciplemanagemenet-pyq.html') 
+    return render(request, '../templates/pyqs/bba pyqs/bba-sem1-priciplemanagemenet-pyq.html') 
 def bbasem2buisnesscommunication(request):
-    return render(request, '../templates/bba-sem2-businnescommunication-pyq.html') 
+    return render(request, '../templates/pyqs/bba pyqs/bba-sem2-businnescommunication-pyq.html') 
 def bbasem2humanresource(request):
-    return render(request, '../templates/bba-sem2-humanresource-pyq.html') 
+    return render(request, '../templates/pyqs/bba pyqs/bba-sem2-humanresource-pyq.html') 
 def bbasem2macroeconomics(request):
-    return render(request, '../templates/bba-sem2-macroeconomics-pyq.html') 
+    return render(request, '../templates/pyqs/bba pyqs/bba-sem2-macroeconomics-pyq.html') 
 def bbasem2origanization(request):
-    return render(request, '../templates/bba-sem2-organizationalbehavior-pyq.html') 
+    return render(request, '../templates/pyqs/bba pyqs/bba-sem2-organizationalbehavior-pyq.html') 
 def bbasem3buisnesslaw(request):
-    return render(request, '../templates/bba-sem3-businesslaw-pyq.html') 
+    return render(request, '../templates/pyqs/bba pyqs/bba-sem3-businesslaw-pyq.html') 
 def bbasem3corporateaccoun(request):
-    return render(request, '../templates/bba-sem3-corporateaccounting-pyq.html') 
+    return render(request, '../templates/pyqs/bba pyqs/bba-sem3-corporateaccounting-pyq.html') 
 def bbasem3managementaccount(request):
-    return render(request, '../templates/bba-sem3-managementaccounting-pyq.html') 
+    return render(request, '../templates/pyqs/bba pyqs/bba-sem3-managementaccounting-pyq.html') 
 def bbasem3marketing(request):
-    return render(request, '../templates/bba-sem3-marketing-pyq.html') 
+    return render(request, '../templates/pyqs/bba pyqs/bba-sem3-marketing-pyq.html') 
 def bbasem4businesseco(request):
-    return render(request, '../templates/bba-sem4-businesseconomics-pyq.html') 
+    return render(request, '../templates/pyqs/bba pyqs/bba-sem4-businesseconomics-pyq.html') 
 def bbasem4costaccounting(request):
-    return render(request, '../templates/bba-sem4-costaccounting-pyq.html') 
+    return render(request, '../templates/pyqs/bba pyqs/bba-sem4-costaccounting-pyq.html') 
 def bbasem4production(request):
-    return render(request, '../templates/bba-sem4-production-pyq.html') 
+    return render(request, '../templates/pyqs/bba pyqs/bba-sem4-production-pyq.html') 
 def bbasem4quantitative(request):
-    return render(request, '../templates/bba-sem4-quantitativetechniques-pyq.html') 
+    return render(request, '../templates/pyqs/bba pyqs/bba-sem4-quantitativetechniques-pyq.html') 
 def bbasem5enterpreneuship(request):
-    return render(request, '../templates/bba-sem5-enterpreneurship-pyq.html') 
+    return render(request, '../templates/pyqs/bba pyqs/bba-sem5-enterpreneurship-pyq.html') 
 def bbasem5internationalbusiness(request):
-    return render(request, '../templates/bba-sem5-internationalbusiness-pyq.html') 
+    return render(request, '../templates/pyqs/bba pyqs/bba-sem5-internationalbusiness-pyq.html') 
 def bbasem5investmentmanagement(request):
-    return render(request, '../templates/bba-sem5-investmentmanagement-py.html') 
+    return render(request, '../templates/pyqs/bba pyqs/bba-sem5-investmentmanagement-py.html') 
 def bbasem5strategicmanagement(request):
-    return render(request, '../templates/bba-sem5-startegicmanagement-pyq.html') 
+    return render(request, '../templates/pyqs/bba pyqs/bba-sem5-startegicmanagement-pyq.html') 
 def bbasem6finacialmanagement(request):
-    return render(request, '../templates/bba-sem6-finacialmanagement-pyq.html') 
+    return render(request, '../templates/pyqs/bba pyqs/bba-sem6-finacialmanagement-pyq.html') 
 def bbasem6marketing(request):
-    return render(request, '../templates/bba-sem6-marketingresearch-pyq.html') 
+    return render(request, '../templates/pyqs/bba pyqs/bba-sem6-marketingresearch-pyq.html') 
 def bbasem6organization(request):
-    return render(request, '../templates/bba-sem6-organizationaldevelopment-pyq.html') 
+    return render(request, '../templates/pyqs/bba pyqs/bba-sem6-organizationaldevelopment-pyq.html') 
 
 
 
